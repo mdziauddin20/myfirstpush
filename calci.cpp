26d933f90eda5c170f1809ba43454e8e9f2d1b18
Md Ziauddin <ziauddinloam@gmail.com>
2025-10-04 22:49:30 +0530

    Add files via upload

---
+++ calci.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a;
+int b;
+cout<<"enter the value of a"<<endl;
+
+cin>>a;
+cout<<"enter the value of b"<<endl;
+cin>>b;
+cout<< "The value of a + b is"<< a + b<<endl;
+cout<<"  the value of a-b is "<< a-b <<endl;
+cout<<" the value of a*b is " << a*b <<endl;
+cout<<" the value of a/b is " <<a/b <<endl;
+
+
+}
