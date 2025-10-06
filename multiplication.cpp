f0c6b8e66c76bbe34a533e07379ebeb7a9d978e2
Md Ziauddin <ziauddinloam@gmail.com>
2025-10-06 23:40:53 +0530

    Add files via upload

---
+++ multiplication.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+   for ( int i = 1; i <= 10; i++)
+   {
+    a=6*i;
+  cout<< " 6 x " <<i <<"= " <<a<<endl;
+   }
+   return 0;
+    
+}
+
+
+
