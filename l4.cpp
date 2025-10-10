93ed5247bd1263d6a1c643eaa7736e8dbf09f197
Md Ziauddin <ziauddinloam@gmail.com>
2025-10-10 23:50:46 +0530

    Add files via upload

---
+++ l4.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+int glo = 6;
+void sum(){
+    int a;
+    cout<<glo;
+}
+
+int main(){
+    int glo=9;
+    glo=78;
+   // int a = 14;
+   // int b = 15;
+   int a=14, b=15;
+   float pi=3.15;
+   char c ='d';
+   bool is_true = false;
+   sum();
+   cout<<glo;
+   cout<<"\nThe value of pi is: "<<pi;
+    return 0;
+
+}
+
+
+    
