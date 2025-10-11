b573f67c0c5325d23c1d659d31cb4d738c3b06bb
Md Ziauddin <ziauddinloam@gmail.com>
2025-10-11 21:44:25 +0530

    Add files via upload

---
+++ structures,union&enums.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main(){
+    struct employe
+    {
+        /* data */
+        int eid;
+        char favchar;
+        float salary;
+    };
+    int main(){
+    struct employee harry;
+    harry.eid=1;
+    harry.fav char='c';
+    harry.salary= 120000000;
+    cout<<"the value is "<<harry.eid<<endl;
+     cout<<"the value is "<<harry.fav char<endl;
+      cout<<"the value is "<<harry.salary<<endl;
+
+    {
+        /* data */
+    };
+    
+
+    }
+    
+    
+    return 0;
+}
