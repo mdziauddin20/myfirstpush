3e2a62bb83675444fe647ea7c8f5369bb6c9431e
Md Ziauddin <ziauddinloam@gmail.com>
2025-10-24 22:54:59 +0530

    Add files via upload

---
+++ passingobjects.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+class complex{
+    int a;
+    int b;
+    void setdata(int v1,int v2){
+        a=v1;
+        b=v2;
+    }
+    void setdatabysum(complex o1,complex o2){
+        a=o1.a + o2.a;
+        b=o1.a+o2.b;
+
+    }
+    void printnumber(){
+        cout<<"your complex number is "<<a<<" + "<<b<<"i"<<endl;
+
+    }
+
+};
+int main(){
+    complex c1,c2,c3;
+    c1.setdata(1,2);
+    c1.printnumber();
+    
+    return 0;
+}
