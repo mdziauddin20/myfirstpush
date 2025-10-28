57fb7e3a2ea3f58a96cb080f3c56637cc7064050
Md Ziauddin <ziauddinloam@gmail.com>
2025-10-28 23:46:36 +0530

    updated friendclass.cpp

--- friendclass.cpp
+++ friendclass.cpp
@@ -1,6 +1,21 @@
 #include<iostream>
 using namespace std;
 int main(){
+    class complex
+{
+    int a, b;
+
+public:
+    void setnumber(int n1, int n2)
+    {
+        a = n1;
+        b = n2;
+    }
     
+    void printnumber()
+    {
+        cout << "your number is " << a << " + " << b << "i" << endl;
+    }
+};
     return 0;
 }
