105f7619e69cda3278242a5ee6aad0fcaa02fb69
Md Ziauddin <ziauddinloam@gmail.com>
2025-11-11 23:37:43 +0530

    Add files via upload

--- friendclass.cpp
+++ friendclass.cpp
@@ -1,10 +1,22 @@
 #include<iostream>
 using namespace std;
-int main(){
-    class complex
-{
-    int a, b;
+class complex;
 
+class calculator{
+    public:
+    int add(int a,int b){
+        return(a+b);
+    }
+    int sumrealcomplex(complex ,complex ){
+        
+    }
+
+};
+
+    class complex{
+        int a,b;
+        friend int calculator::sumrealcomplex(complex,complex);
+    
 public:
     void setnumber(int n1, int n2)
     {
@@ -17,5 +29,17 @@ public:
         cout << "your number is " << a << " + " << b << "i" << endl;
     }
 };
+int calculator::sumrealcomplex(complex o1,complex o2 ){
+    return(o1.a+o2.a);
+
+}
+int main (){
+
+  complex o1,o2;
+  o1.setnumber(1,4);
+  o2.setnumber(5,7);
+  calculator calc;
+  int res =calc.sumrealcomplex(o1,o2);
+  cout<<"the sum of real part of o1and o2 is "<<res<<endl;
     return 0;
 }
